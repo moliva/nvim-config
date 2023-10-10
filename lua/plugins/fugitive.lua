8ffeec1c7abe286a0b2bc1b1265ad65bc4f91b75
moliva <oliva.miguelh@gmail.com>
2023-10-10 20:52:08 -0300

    fugitive git push

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
   'tpope/vim-fugitive',
   cmd = "Git",
   keys = {
-    { "<leader>gs", vim.cmd.Git, desc = "Git Status" }
+    { "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+    { "<leader>gp", '<cmd>Git push origin HEAD<cr>', desc = "Git Push" },
   }
 }
