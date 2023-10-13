2bd4c90dde1c52f370ab97d26127c10ac0f7ee1a
moliva <oliva.miguelh@gmail.com>
2023-10-13 17:59:44 -0300

    moving snippets to its own file

---
+++ lua/kmobic33/snippets.lua
@@ -0,0 +1,98 @@
+local M = {}
+
+function M.configure_snippets(ls)
+  local s = ls.snippet
+  -- local sn = ls.snippet_node
+  -- local isn = ls.indent_snippet_node
+  local t = ls.text_node
+  local i = ls.insert_node
+  -- local f = ls.function_node
+  local c = ls.choice_node
+  -- local d = ls.dynamic_node
+  -- local r = ls.restore_node
+
+  -- local events = require("luasnip.util.events")
+  -- local ai = require("luasnip.nodes.absolute_indexer")
+
+  local extras = require("luasnip.extras")
+  -- local l = extras.lambda
+  -- local rep = extras.rep -- repeats the content of the `number` insert node referenced
+  -- local p = extras.partial
+  -- local m = extras.match
+  -- local n = extras.nonempty
+  -- local dl = extras.dynamic_lambda
+
+  local fmt = require("luasnip.extras.fmt").fmt
+  -- local fmta = require("luasnip.extras.fmt").fmta
+  -- local conds = require("luasnip.extras.expand_conditions")
+  -- local postfix = require("luasnip.extras.postfix").postfix
+  -- local types = require("luasnip.util.types")
+  local parse = require("luasnip.util.parser").parse_snippet
+
+  -- cleans up all snippets (useful when reloading this file)
+  ls.cleanup()
+
+  ls.add_snippets("all", {
+    parse("separator",
+      "$LINE_COMMENT *****************************************************************************************************\n$LINE_COMMENT *************** ${1:separator} ***************\n$LINE_COMMENT *****************************************************************************************************\n"),
+    parse("todo",
+      "$LINE_COMMENT ${1|TODO,FIXME,XXX|} - ${2:description} - moliva - $CURRENT_YEAR/$CURRENT_MONTH/$CURRENT_DATE"),
+    -- s("todo1", fmt("{} {} - {} - moliva - {}", { t("//"), c(1, { t("TODO"), t("FIXME"), t("XXX") }), i(2, "description"), f(function()
+    --   return
+    --       os.date("%Y/%m/%d")
+    -- end) })),
+  })
+
+  local languages = { "typescript", "typescriptreact" }
+  for _, lang in ipairs(languages) do
+    ls.add_snippets(lang, {
+      parse("log",
+        "console.log('${1:expression}', $1)"),
+    })
+  end
+
+
+  ls.add_snippets("rust", {
+    s("modtest",
+      fmt(
+        [[
+            #[cfg(test)]
+            mod test {{
+            {}
+
+              {}
+            }}
+   ]],
+        {
+          c(1, { t("  use super::*;", t("")) }),
+          i(0)
+        }))
+  })
+
+  ls.add_snippets("text", {
+    s("meeting", fmt("**** {} ****\n{}", { i(1, "meeting name"), i(0) })),
+    s("newmeetingsfile", fmt(
+      [[
+
+    {}
+
+
+    ****************************************************************************************************************
+    ********************* Action items *********************
+    ****************************************************************************************************************
+
+
+
+    EOF
+    ]], { i(0) })),
+    parse("today", "$CURRENT_YEAR/$CURRENT_MONTH/$CURRENT_DATE"),
+    s("sep", fmt(
+      [[
+    ****************************************************************************************************************
+    ********************* {} *********************
+    ****************************************************************************************************************
+    ]], { i(0) })),
+  })
+end
+
+return M
