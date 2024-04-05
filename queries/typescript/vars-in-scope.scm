99501d00509fc143d2b05683ad3716df6ebfceb4
moliva <oliva.miguelh@gmail.com>
2024-04-05 11:29:56 -0300

    feat: improving forconst snippet

--- queries/typescript/vars-in-scope.scm
+++ queries/typescript/vars-in-scope.scm
@@ -1,5 +1,15 @@
 [(function_declaration 
-   parameters: (formal_parameters (required_parameter pattern: (_) @param)) 
+   parameters: (formal_parameters (required_parameter pattern: (_) @param))?
    body: (statement_block (lexical_declaration
-        (variable_declarator name: (_) @identifier)))
+        (variable_declarator name: (_) @identifier)))?
+)
+(function_expression 
+   parameters: (formal_parameters (required_parameter pattern: (_) @param))?
+   body: (statement_block (lexical_declaration
+        (variable_declarator name: (_) @identifier)))?
+)
+(arrow_function 
+   parameters: (formal_parameters (required_parameter pattern: (_) @param))?
+   body: (statement_block (lexical_declaration
+        (variable_declarator name: (_) @identifier)))?
 )]
